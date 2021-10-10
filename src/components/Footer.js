24e02a55559b0e00d1e8d0dc56943551794b72aa
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 01:09:15 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,8 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Made By Forhatul Hasan Tamim</h3>
+<h3>Designed and Developed by Soumyajit Behera</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} the_fhtamim, LLC</h3>

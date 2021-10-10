8c57f8351309c1a3c0ce79e817178ff9049666dc
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 00:21:01 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,8 +69,8 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-
-           <Nav.Item>
+         {/*  
+            <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/project"
@@ -82,7 +82,7 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item> 
-            
+            */}
 
             <Nav.Item>
               <Nav.Link

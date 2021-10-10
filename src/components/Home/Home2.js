78876a016a5a82ac8407051b2c962fe002553c3a
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-10 23:55:36 +0600

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/fhtamim/"
+                  href="https://www.linkedin.com/in/f-h-tamim/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"

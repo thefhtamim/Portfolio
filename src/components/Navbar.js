b39525c2c17ec3e3e0c7fae4e9c2c075311cc7c9
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 01:32:02 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ import {
   AiFillStar,
   AiOutlineHome,
  // AiOutlineFundProjectionScreen,
- // AiOutlineUser,
+  AiOutlineUser,
 } from "react-icons/ai";
 
 //import { CgFileDocument } from "react-icons/cg";
@@ -59,7 +59,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-{/*     <Nav.Item>
+     <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/about"
@@ -68,7 +68,7 @@ function NavBar() {
                 <AiOutlineUser style={{ marginBottom: "2px" }} /> About
               </Nav.Link>
             </Nav.Item>
-            */}
+            
 
          {/*  
             <Nav.Item>

5ecc69b5b4e5558a9a021c3db3a093716c028090
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 01:47:49 +0600

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
-import Techstack from "./Techstack";
+//import Github from "./Github";
+//import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
+//import Toolstack from "./Toolstack";
 
 function About() {
   return (
@@ -26,7 +26,7 @@ function About() {
             </h1>
             <Aboutcard />
           </Col>
-          <Col
+{/*   <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
@@ -45,7 +45,7 @@ function About() {
         </h1>
         <Toolstack />
 
-        <Github />
+        <Github />  */}
       </Container>
     </Container>
   );

669502f3c50b592eca458be875eaff3fb8f64f95
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 00:13:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
+         // <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
         </Switch>

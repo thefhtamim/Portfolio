030201d4ddd2ac842d07a1cc34694bd108188c51
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-10 23:56:39 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Soumyajit Behera</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} the_fhtamim LLC</h3>
+          <h3>Copyright © {year} the_fhtamim, LLC</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">

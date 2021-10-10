54207523aaf3539770fcef135c0275df5d1802d8
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 01:05:58 +0600

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+//import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -27,9 +27,9 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+          {/*  <Col md={5} style={{ paddingBottom: 20 }}>
               <img src={homeLogo} alt="home pic" className="img-fluid" />
-            </Col>
+            </Col>  */}
           </Row>
         </Container>
       </Container>

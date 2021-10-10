54104fdd67cb440f0a8bee23547f79f6e7774bf8
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 02:13:33 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
-//import { CgFileDocument } from "react-icons/cg";
+import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -85,7 +85,7 @@ function NavBar() {
             </Nav.Item> 
             */}
 
-{/*  <Nav.Item>
+  <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -94,7 +94,7 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-*/}
+
             <Nav.Item>
               <Nav.Link
                 href="https://www.fhtamim.me"

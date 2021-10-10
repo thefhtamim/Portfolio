6528eb1b004e783a6308152de7eba4e223a05536
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 02:49:00 +0600

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,6 +1,6 @@
 
 
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 //import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
@@ -9,28 +9,28 @@ import axios from "axios";
 //import pdf from "../../Assets/Soumyajit-Behera.pdf";
 //import { AiOutlineDownload } from "react-icons/ai";
 
-function Resume() {
-  const uri = "https://porfolio-backend.vercel.app/ranks/getRanks";
-  const [spojRank, upadteSpojRank] = useState(0);
-  const [hackerrank, upadteHackerank] = useState(0);
+// function Resume() {
+ // const uri = "https://porfolio-backend.vercel.app/ranks/getRanks";
+ // const [spojRank, upadteSpojRank] = useState(0);
+ // const [hackerrank, upadteHackerank] = useState(0);
  // const [sem, upadateSem] = useState(0);
  // const [cgpa, upadteCgpa] = useState(0);
 
-  useEffect(() => {
-    axios
-      .get(uri)
-      .then((res) => {
-         upadteSpojRank(res.data.message[0].spojRank);
-        upadteHackerank(res.data.message[1].hackerrank);
+//  useEffect(() => {
+ //   axios
+  //    .get(uri)
+   //   .then((res) => {
+   //      upadteSpojRank(res.data.message[0].spojRank);
+  //      upadteHackerank(res.data.message[1].hackerrank);
        // upadteCgpa(res.data.message[2].cgpa);
        // upadateSem(res.data.message[3].sem);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+ //     })
+ //     .catch((err) => {
+//        console.log(err);
+ //     });
+//  }, []);
 
-  return (
+//  return (
     <Container fluid className="resume-section">
       <Particle />
       <Container>

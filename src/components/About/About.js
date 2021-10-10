53d34295c6aaa8a84c028d0c147a258b956d64a8
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-10-11 02:06:24 +0600

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 //import Github from "./Github";
 //import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+//import laptopImg from "../../Assets/about.png";
 //import Toolstack from "./Toolstack";
 
 function About() {
@@ -26,13 +26,14 @@ function About() {
             </h1>
             <Aboutcard />
           </Col>
-   <Col
+{/* <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
             <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
+          </Col> 
+          */}
         </Row>
 {/*
         <h1 className="project-heading">
